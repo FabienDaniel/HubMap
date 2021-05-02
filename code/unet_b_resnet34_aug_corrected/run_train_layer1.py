2ab8a8f07f3a8fbed7c6ae12a08573f71cb71486
fabien <fabien128@gmail.com>
2021-05-02 16:31:23 +0200

    [FEAT] layer1, change seed

--- code/unet_b_resnet34_aug_corrected/run_train_layer1.py
+++ code/unet_b_resnet34_aug_corrected/run_train_layer1.py
@@ -540,7 +540,7 @@ def run_train(show_valid_images=False,
 if __name__ == '__main__':
 
     # Setting seed
-    seed = 34
+    seed = 35
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
     np.random.seed(seed)
